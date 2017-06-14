e7eda65d17291d4ab12310d71d0d0a037bbb3e02
David Herrin <david_herrin@hotmail.com>
2017-06-14 17:23:24 -0500

    required 5:00 pm save

--- cli/src/Message.js
+++ cli/src/Message.js
@@ -3,21 +3,48 @@ export class Message {
     return new Message(JSON.parse(buffer.toString()))
   }
 
-  constructor ({ username, command, contents }) {
+  constructor ({ username, command, contents, timestamp }) {
     this.username = username
     this.command = command
     this.contents = contents
+    this.timestamp = timestamp
   }
 
   toJSON () {
     return JSON.stringify({
       username: this.username,
       command: this.command,
-      contents: this.contents
+      contents: this.contents,
+      timestamp: this.timestamp
     })
   }
 
   toString () {
+    const chalk = require('chalk')
+    if (this.command === 'connect') {
+      return (chalk.yellow)(`${this.timestamp}: <${this.username}> has connected`)
+    }
+
+    if (this.command === 'disconnect') {
+      return (chalk.yellow)(`${this.timestamp}: <${this.username}> has disconnected`)
+    }
+
+    if (this.command === 'echo') {
+      return (chalk.red)(`${this.timestamp}: <${this.username}> (echo): ${this.contents}`)
+    }
+
+    if (this.command === 'users') {
+      return (chalk.blue)(`${this.timestamp}: currently connected users: ${this.contents}`)
+    }
+
+    if (this.command === 'broadcast') {
+      return (chalk.magenta)(`${this.timestamp} <${this.username}> (all): ${this.contents}`)
+    }
+
+    if (this.command.charAt(0) === '@') {
+      return (chalk.orange)(`${this.timestamp} <${this.username}> (whisper): ${this.contents}`)
+    }
+
     return this.contents
   }
 }
--- src/main/java/com/cooksys/assessment/server/ClientHandler.java
+++ src/main/java/com/cooksys/assessment/server/ClientHandler.java
@@ -1,11 +1,17 @@
 package com.cooksys.assessment.server;
 
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +21,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ClientHandler implements Runnable {
 	private Logger log = LoggerFactory.getLogger(ClientHandler.class);
+	public static Map<String, Socket> clientList = new HashMap<>();
 
 	private Socket socket;
 
@@ -33,20 +40,63 @@ public class ClientHandler implements Runnable {
 			while (!socket.isClosed()) {
 				String raw = reader.readLine();
 				Message message = mapper.readValue(raw, Message.class);
-
+				Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+				message.setTimestamp(timestamp.toString());
+				String response;
+				
+				if (message.getCommand().charAt(0) == '@') {
+					String[] chopCommand = message.getCommand().split("\\s");
+					log.info("user <{}> message <{}> to <{}>", message.getUsername(), chopCommand[1], chopCommand[0]);
+				}
+				
 				switch (message.getCommand()) {
 					case "connect":
 						log.info("user <{}> connected", message.getUsername());
+						clientList.put(message.getUsername(), this.socket);
+					    for (Socket value : clientList.values()) {
+					        PrintWriter conWriter = new PrintWriter(new OutputStreamWriter((value).getOutputStream()));
+					        response = mapper.writeValueAsString(message);
+					        conWriter.write(response);
+					        conWriter.flush();
+					      }
 						break;
 					case "disconnect":
 						log.info("user <{}> disconnected", message.getUsername());
+						clientList.remove(message.getUsername());
 						this.socket.close();
+						for (Socket value : clientList.values()) {
+					        PrintWriter disconWriter = new PrintWriter(new OutputStreamWriter((value).getOutputStream()));
+					        response = mapper.writeValueAsString(message);
+					        disconWriter.write(response);
+					        disconWriter.flush();
+					      }
 						break;
 					case "echo":
 						log.info("user <{}> echoed message <{}>", message.getUsername(), message.getContents());
-						String response = mapper.writeValueAsString(message);
+						response = mapper.writeValueAsString(message);
 						writer.write(response);
 						writer.flush();
+						break;
+					case "users":
+						log.info("user <{}> users", message.getUsername());
+						List<String> userNames = new ArrayList<String>(clientList.keySet());
+						String outNameList = "";
+						for (String name : userNames) {
+							outNameList += "\n" + "<" + name + ">";
+						}
+						message.setContents(outNameList);
+						response = mapper.writeValueAsString(message);
+						writer.write(response);
+						writer.flush();
+						break;
+					case "broadcast":
+						log.info("user <{}> broadcast <{}>", message.getUsername(), message.getContents());
+						for (Socket value : clientList.values()) {
+					        PrintWriter bcastWriter = new PrintWriter(new OutputStreamWriter((value).getOutputStream()));
+					        response = mapper.writeValueAsString(message);
+					        bcastWriter.write(response);
+					        bcastWriter.flush();
+					      }
 						break;
 				}
 			}
